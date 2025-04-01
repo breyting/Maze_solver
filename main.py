04c1d5915be23bc1c50854c4556dc2cdfe1f3e8d
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-04-01 20:41:56 +0200

    adding the entry and exit + test of it

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 from graphics import Window, Line, Point, Cell
+from maze import Maze
 
 
-def main():
-    win = Window(800, 600)
-    
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+def main():    
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(125, 125, 200, 200)
-
-    c2 = Cell(win)
-    c2.has_bottom_wall = False
-    c2.draw(225, 225, 250, 250)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.draw(300, 300, 500, 500)
-
-    c.draw_move(c2)
-    c3.draw_move(c1, True)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
